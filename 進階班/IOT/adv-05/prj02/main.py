dd83ea8344f38db7ae12779e6f088a3c477b6f37
gamerlawrenceho <gamerlawrenceho@gmail.com>
2024-03-31 10:56:49 +0800

    adv05

---
+++ 進階班/IOT/adv-05/prj02/main.py
@@ -0,0 +1,31 @@
+#########################匯入模組#########################
+import socket
+
+#########################函式與類別定義#########################
+
+#########################宣告與設定#########################
+HOST = "localhost"
+PORT = 5487
+server_socket = socket.socket()
+server_socket.bind((HOST, PORT))
+server_socket.listen(5)
+print("server:{} port:{} start".format(HOST, PORT))
+client, addr = server_socket.accept()
+print("client address:{}, port:{}".format(addr[0], addr[1]))
+#########################主程式#########################
+while True:
+    msg = client.recv(128).decode("utf8")
+    print("Receive Message:" + msg)
+    reply = ""
+
+    if msg == "Hi":
+        reply = "Hello!"
+        client.send(reply.encode("utf8"))
+    elif msg == "Bye":
+        client.send(b"quit")
+        break
+    else:
+        reply = "what??"
+        client.send(reply.encode("utf8"))
+client.close()
+server_socket.close()
