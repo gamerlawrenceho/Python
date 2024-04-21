1f0f18c533e78a77092e408ffa651b3e09b264d2
gamerlawrenceho <gamerlawrenceho@gmail.com>
2024-04-21 11:26:28 +0800

    adv06

---
+++ 進階班/IOT/adv-06/prj03/main.py
@@ -0,0 +1,28 @@
+#########################匯入模組#########################
+import paho.mqtt.client as mqtt
+
+#########################函式與類別定義#########################
+def on_connect(client, userdata, connect_flags, reason_code, properties):
+    print(f"連線結果:{reason_code}")
+    client.subscribe("hello")  # 訂閱主題
+
+def on_message(client, userdata, msg):
+    print(f"我訂閱的主題是:{msg.topic}, 收到訊息:{msg.payload.decode('utf-8')}")
+
+#########################宣告與設定#########################
+# 建立客戶端實例
+client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
+client.on_connect = on_connect
+client.on_message = on_message
+client.username_pw_set("singular", "Singular#1234")
+client.connect("mqtt.singularinnovation-ai.com", 1883, 60)
+client.loop_forever()
+
+
+
+
+
+
+
+
+
